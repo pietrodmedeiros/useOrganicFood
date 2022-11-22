b08790ebeb4ca0dd96c9b58f1ead2a17d8e5b673
pietrodmedeiros <spogmedeiros@gmail.com>
2022-11-21 22:04:44 -0300

    criar tabela horata e salvar tabela horta

--- connect_db/connect_db.js
+++ connect_db/connect_db.js
@@ -1,16 +1,15 @@
 var db = window.openDatabase("OrganicFood Banco", "2.0", "Mybase", 4048);
 db.transaction(function(criar){
-    criar.executeSql("CREATE TABLE users (ID PRIMARY KEY, nome TEXT, senha TEXT)");
-
+    criar.executeSql("CREATE TABLE IF NOT EXISTS hortas(nome, senha, email, endereco)");
 });
 
-
-function salvar() {
+function salvarHorta() {
     var nome = document.getElementById('nome').value;
     var senha = document.getElementById('senha').value;
-
+    var email = document.getElementById('email').value;
+    var endereco = document.getElementById('logradouro').value;
+    var values = [nome,senha,email,endereco];
     db.transaction(function(armazenar){
-        armazenar.executeSql("INSERT INTO users (nome, senha) VALUES (?,?)", [nome,senha]);
-
+        armazenar.executeSql("INSERT INTO hortas(nome, senha, email, endereco) VALUES (?,?,?,?)", values );
     });
 }
